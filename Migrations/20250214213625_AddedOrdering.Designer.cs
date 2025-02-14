e08197345947a53eb27d42b96875a7632fcdc1a4
StevenCoverdale <S.coverdale@hotmail.com>
2025-02-14 16:37:08 -0500

    Added Ordering
    Also updated the database

--- Migrations/20250214213625_AddedOrdering.Designer.cs
+++ Migrations/20250214213625_AddedOrdering.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using assignment1.Data;
@@ -11,9 +12,11 @@ using assignment1.Data;
 namespace assignment1.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250214213625_AddedOrdering")]
+    partial class AddedOrdering
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
