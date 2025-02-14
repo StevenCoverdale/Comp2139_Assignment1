d73f28c57cfbc01d9a4aa7194f32d23416f2251c
David Rashidi <moewo@Davids-MacBook-Pro.local>
2025-02-13 20:31:08 -0500

    Add Product Model, Product View, and Product Controller with test data

--- Program.cs
+++ Program.cs
@@ -24,4 +24,11 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Product}/{action=Index}/{id?}");
+});
+
 app.Run();
